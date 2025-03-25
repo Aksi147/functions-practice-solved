510f1b336f4fcc76bdc280f0516cc441becac4fd
Aksi147 <akjr820@gmail.com>
2025-03-24 23:35:45 -0400

    worked on exercises

--- exercises/e4.js
+++ exercises/e4.js
@@ -9,10 +9,11 @@
  * splitFirstAndLastNames('John Smith') => ['John', 'Smith']
  */
 
-function splitFirstAndLastNames(str) {
+/* function splitFirstAndLastNames(str) {
   return str.split(' ');
-}
+} */
 
+const splitFirstAndLastNames = (str) => str.split(" ");
 
 /** =========================
  * The personAge() constant is assigned to a function, that takes an object of person Data as the argument.
@@ -20,10 +21,11 @@ function splitFirstAndLastNames(str) {
  * personAge({ id: 1, name: 'Someone', age: 32}) => 32
  */
 
-const personAge = function(personObject) {
+/* const personAge = function(personObject) {
   return personObject.age;
-}
+} */
 
+let personAge = (personObject) => personObject.age;
 
 /** =========================
  * The isNameInArray function takes an array and string of name as the arguments.
@@ -32,10 +34,7 @@ const personAge = function(personObject) {
  * isNameInArray(['Jon', 'Michael', 'Andrey'], 'James') => false;
  */
 
-const isNameInArray = function(arr, name) {
-  return arr.includes(name);
-}
-
+const isNameInArray = (arr, name) => arr.includes(name);
 
 /** =========================
  * The logTimer function takes a number as the arguments.
@@ -44,17 +43,27 @@ const isNameInArray = function(arr, name) {
  * !!! Hint: There are two functions to refactor !!!
  */
 
-const logSecondsUpToMax = function(max) {
+/* const logSecondsUpToMax = function (max) {
   let i = 0;
-  const timer = setInterval(function() {
+  const timer = setInterval(function () {
     if (i < max) {
       console.log(++i);
     } else {
       clearInterval(timer);
     }
   }, 1000);
-}
+}; */
 
+const logSecondsUpToMax = (max) =>
+  let i = 0;
+  const timer = setInterval(() => {
+    if (i < max) {
+      console.log(++i);
+    } else {
+      clearInterval(timer);
+    }
+  }, 1000);
+};
 
 
 // === TEST YOURSELF ===
@@ -62,12 +71,11 @@ const logSecondsUpToMax = function(max) {
 // If the test has all tests passed, switch to the next exercise file
 // If any of the tests fails, refactor the code and run the test command after you've fixed the function
 
-
 // Do not delete or change the lines beneath
 const f = {
   splitFirstAndLastNames: splitFirstAndLastNames || undefined,
   personAge: personAge || undefined,
   isNameInArray: isNameInArray || undefined,
   logSecondsUpToMax: logSecondsUpToMax || undefined,
-}
-export { f }
+};
+export { f };
